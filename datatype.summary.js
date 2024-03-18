3836a810dd48322001b5bf121ebfe5e545b2d5ed
SimranKumari01 <kumarisimmo077@gmail.com>
2024-03-18 23:40:33 +0530

    datatype.summary.js

---
+++ datatype.summary.js
@@ -0,0 +1,28 @@
+//primitive -
+//7 type- String, number, boolean, null, undefined, symbol, bigint, 
+//refernce type (nonprimitive)
+//array, objects, functions
+
+const score = 100
+const  scorevalue= 100.3
+
+constisLoggedIn=false
+const outsideTemp = null
+let useremail;
+
+const id=symbol('1,2,3')
+const anotherId = symbol('1,2,3')
+console.log(id === anotherId);
+
+const bigNumber = 345678967890n
+const heros =["shaktiman", "naagraj", "doga"]
+let myObj={
+    name:"simran",
+    age: 22,
+}
+const myfunction=function(){
+    console.log("hello simran")
+}
+console.log(typeof myfunction);
+
+
---
+++ xyz.js
@@ -0,0 +1,35 @@
+let score="33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33=>33
+//"33abc"=>NAN
+//true=>1; false=>0
+
+
+let isLoggedIn="1"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=true; 0=false
+//""= false
+//"simran"=true
+
+let someNumber=33
+
+letstringNumber=string(someNumber)
+console.log(stringnumber);
+console.log(typeofstringnumber);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log ((3+4) *5 % 3);
